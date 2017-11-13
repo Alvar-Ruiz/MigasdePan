c2b4deadbab8452da28b1dfd75d1391ed9b5ed75
Álvar Ruiz <alvar.ruiz.82@gmail.com>
2017-11-13 20:23:12 +0100

    Revisión

--- js/MigasdePan.js
+++ js/MigasdePan.js
@@ -1,10 +1,9 @@
-function generateBC(url, separador) 
-{
+function generateBC(url, separador) {
     // Variables:
     const CLASS = "active";     // Constante que almacena el valor de la clase atribuida al contenedor <span> que forma la última miga.
     var urlProcesada = "";      // Contendrá la URL procesada de forma que sólo contenga los elementos que formarán la migas de pan. 
     var componentesUrl = [];    // Array que contendrá los elementos que formarán el contenido de las migas de pan.
-    var migas = [];             // Array que contendrá las migas de pan ya formadas.
+    var migas = [];             // Array que contendrá las migas de pan ya formadas con las etiquetas <a> y <span>.
  
     // 1) Proceso la URL en bruto recibida como parámetro:
     urlProcesada = url.replace(/^(http(s)?:\/\/)?(www\.)?.[^\/]+(\.[a-z0-9]+)*/i, "HOME"). // Reemplazo la primera parte de la URL por "HOME".
@@ -20,10 +19,10 @@ function generateBC(url, separador)
     /* 3) Creo las migas de pan a partir de los componentes. De la primera a la penúltima miga serán creadas con la función crearEnlace(), 
     mientras que la última miga se creará con la función crearUltimaMiga(): */
     
-    // 3.1) Si la URL contenía más un componente, genero todas la migas excepto la última mediante un bucle for:
+    // 3.1) Si la URL contiene más un componente, genero todas la migas excepto la última mediante un bucle for:
     if (componentesUrl.length > 1) {
         
-        let ruta = "/"; // Almacena la ruta que tendrá el atributo href de cada miga.
+        let ruta = "/"; // Variable que almacenará la ruta que tendrá el atributo href de cada miga.
         
         // Construyo y añado al array de migas la primera miga, que será HOME con href="/":
         migas.push(crearEnlace(ruta, componentesUrl[0]));
@@ -42,7 +41,7 @@ function generateBC(url, separador)
         }
     }
     
-    /* 3.2) Creo la última miga, o la única miga si la URL sólo contenía un único componente. Del mismo modo, 
+    /* 3.2) Creo la última miga, o la única miga si la URL sólo contiene un único componente. Del mismo modo, 
     proceso el componenete con la función procesarComponente(componenteUrl), y si el componente tiene más de 30 caracteres, 
     elaboro un acrónimo con la función acronimo(componenteUrl): */
     if (componentesUrl[componentesUrl.length - 1].length <= 30) {
@@ -59,20 +58,18 @@ function generateBC(url, separador)
 // Funciones Auxiliares    
 
 // Función que recibe un componente, lo pasa a mayúsculas y sustituye por espacios todos los caracteres que no sean letras ni números:
-function procesarComponente(componenteUrl)
-{
+function procesarComponente(componenteUrl) {
     return componenteUrl.toUpperCase().replace(/[^A-Z0-9]/g, " ");
 }
     
 // Función que elabora el acrónimo de un componente:
-function acronimo(componenteUrl, separador)
-{
+function acronimo(componenteUrl, separador) {
     // Variables:
     var palabrasAcronimo = [];  // Array que contendrá las palabras de las cuales se extraerá el acrónimo.
     var letrasAcronimo = [];    // Array que contendrá las letras que formarán el acrónimo.
     var re = new RegExp(separador, "g"); /* Objeto de tipo RegExp que representa la expresión regular que recoge el separador en función del cual se separarán la palabras del componente. */
     
-    /* Extraigo las palabras dividiendo el componente por los guiones (-) y filtrando las palabras no permitidas 
+    /* Extraigo las palabras dividiendo el componente por el separador y filtrando las palabras no permitidas 
     ("the","of","in","from","by","with","and", "or", "for", "to", "at", "a"): */
     palabrasAcronimo = componenteUrl.split(re).filter(palabra => !/^(the|of|in|from|by|with|and|or|for|to|at|a)$/i.test(palabra));
     
@@ -84,15 +81,13 @@ function acronimo(componenteUrl, separador)
 }
  
 // Función que construye y devuelve de la primera a la penúltima miga, recibiendo ruta y contenido y creando el enlace:
-function crearEnlace(ruta, contenido)
-{
+function crearEnlace(ruta, contenido) {
     var miga = '<a href="'.concat(ruta, '">', contenido, '</a>');
     return miga;
 }
     
 // Función que crea y devuelve la última miga del conjunto, formada por un contenedor span y una clase dada:
-function crearUltimaMiga(class_name, contenido)
-{
+function crearUltimaMiga(class_name, contenido) {
     var ultimaMiga = '<span class="'.concat(class_name, '">', contenido, '</span>');
     return ultimaMiga;
 }
